abc716c68f4fdf156eb287e7e81f18dce14d732b
zh <2710383738@qq.com>
2019-12-12 20:52:28 +0800

    遍历数组

--- src/test/java/com/zeroten/flow/IfElseTest.java
+++ src/test/java/com/zeroten/flow/IfElseTest.java
@@ -2,19 +2,19 @@ package com.zeroten.flow;
 
 import org.testng.annotations.Test;
 import java.lang.StringBuffer;
-
+import java.util.Arrays;
 
 
 public class IfElseTest {
 
     @Test
     public void testIfElse() {
-        StringBuffer str = new StringBuffer();
-        str.append("hello");
-        System.out.println(str);
-        System.out.println(str.length());
+        Integer[] arr = {1,2,3,4,5};
+        for (int val1 : arr){
+            System.out.println(val1);
+        }
+        Arrays.asList(arr).forEach(val -> System.out.println(val));
     }
-
 
 
 }
