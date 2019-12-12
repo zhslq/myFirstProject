44d64d5cf34de0d26ca33dc8afbfdb2680208059
zh <2710383738@qq.com>
2019-12-12 20:30:31 +0800

    未修改，仅测试

--- src/test/java/com/zeroten/flow/IfElseTest.java
+++ src/test/java/com/zeroten/flow/IfElseTest.java
@@ -1,12 +1,20 @@
 package com.zeroten.flow;
 
 import org.testng.annotations.Test;
+import java.lang.StringBuffer;
+
+
 
 public class IfElseTest {
 
     @Test
     public void testIfElse() {
-
+        StringBuffer str = new StringBuffer();
+        str.append("hello");
+        System.out.println(str);
+        System.out.println(str.length());
     }
+
+
 
 }
